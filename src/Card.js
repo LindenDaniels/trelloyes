1ab54ed4677e7e4904045a5101d01ae43739c1ad
Linden Daniels <lindendaniels@gmail.com>
2019-09-11 23:07:50 -0400

    Completed update

--- src/Card.js
+++ src/Card.js
@@ -7,6 +7,7 @@ export default function Card(props) {
     <div className='Card'>
       <button
         type='button'
+        onClick = { ()=> props.onClickDelete(props.id)}
       >
         delete
       </button>
@@ -14,4 +15,8 @@ export default function Card(props) {
       <p>{props.content}</p>
     </div>
   )
+}
+
+Card.propTypes = {
+  onClickDelete: () => {}
 }
